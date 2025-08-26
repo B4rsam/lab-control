97a7280c5968b22fe9790790544b3af069866daf
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-08-26 16:52:21 +0330

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends(
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ),
+    {
+        languageOptions: {
+            parser: tsParser,
+        },
+
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "react-hooks/exhaustive-deps": "off",
+        },
+    },
+];
+
+
+export default eslintConfig;
