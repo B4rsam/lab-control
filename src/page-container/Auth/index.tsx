beb9ce720abda5a10f11fcc94f9afb13c0d937ce
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-08-27 14:35:08 +0330

    feat: login styling

--- src/page-container/Auth/index.tsx
+++ src/page-container/Auth/index.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import useViewController from "@/src/page-container/Auth/useViewController";
+import s from "./auth.module.css";
 
 const AuthPageContainer = () => {
     const { onSubmit, onChange } = useViewController();
 
     return (
-        <div className="main-container">
-            <div className="main-box">
+        <div className={s.main_container}>
+            <div className={s.main_box}>
                 <h1>Lab Control</h1>
                 <form onSubmit={onSubmit}>
                     <input name="username" placeholder="Username" type="text" onChange={onChange} />
-                    <input name="password" placeholder="Password" type="password" onChange={onChange} />
-                    <button type="submit" />
+                    <input
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        onChange={onChange}
+                    />
+                    <button type="submit">Login</button>
                 </form>
             </div>
         </div>
