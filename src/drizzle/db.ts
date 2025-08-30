7d53ae0f68581c6f15fca140c71879ac7ac2423b
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-08-30 16:24:23 +0330

    feat: db env

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/d1";
 
 const pool = new Pool({
-    user: "wontwork",
-    password: "1234",
-    database: "labcontrol",
-    host: "localhost",
-    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
 });
 
 export const db = drizzle(pool);
