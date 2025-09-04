f4e6e702e05766fdcc8e41bf6567dbf291b28510
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-09-04 15:52:30 +0330

    feat: landing page structure

--- src/page-container/Landing/index.tsx
+++ src/page-container/Landing/index.tsx
@@ -1,5 +1,16 @@
+import s from "./landing.module.css";
+
 const LandingPageContainer = () => {
-    return <div>Landing Page</div>;
+    return (
+        <div className={s.mainBody}>
+            <div className={s.mainContainer}>
+                <section className={s.titleSection}>
+                    <h1 className={s.mainTitle}>Welcome Username</h1>
+                </section>
+                <section className={s.contentSection}>Content goes here</section>
+            </div>
+        </div>
+    );
 };
 
 export default LandingPageContainer;
